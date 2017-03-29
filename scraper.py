9e4a82e1b791e296288ecc4edb0b60bd0ecbedbf
mdclark23 <Mdclark23@gmail.com>
2017-03-29 12:45:35 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url = https://bestfightodds.com/
+url = 'https://bestfightodds.com/'
 response = requests.get(url, verify=False)
 html = response.content
 
